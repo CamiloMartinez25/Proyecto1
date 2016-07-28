f53b0f18eacdf8647059d4ed7a8d8ab71ed95bbb
Camilo Andres Martinez <camilo_2510@hotmail.com>
2016-07-28 20:28:41 -0300

    Update view2.js

--- javascript/view2.js
+++ javascript/view2.js
@@ -1,18 +1,18 @@
 viewTwo = function(){
 	$("#entradas").on("click", function(){
 		$("#col2").empty();
-		$("#col2").load("entradas.html", entradas);
+		$("#col2").load("views/entradas.html", entradas);
 	});
 	$("#principal").on("click", function(){
 		$("#col2").empty();
-		$("#col2").load("principal.html", entradas);
+		$("#col2").load("views/principal.html", entradas);
 	});
 	$("#postres").on("click", function(){
 		$("#col2").empty();
-		$("#col2").load("postres.html", entradas);
+		$("#col2").load("views/postres.html", entradas);
 	});
 	$("#bebidas").on("click", function(){
 		$("#col2").empty();
-		$("#col2").load("bebidas.html", entradas);
+		$("#col2").load("views/bebidas.html", entradas);
 	});
 }
