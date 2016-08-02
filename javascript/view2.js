87db0721eda8f8e7861ce953b8ec026d6f1030e4
Camilo Andres Martinez <camilo_2510@hotmail.com>
2016-08-02 19:52:08 -0300

    update file

--- javascript/view2.js
+++ javascript/view2.js
@@ -3,7 +3,7 @@ viewTwo = function(){
 		$("#maincol2").removeClass("transitionY rotateYY").addClass("transitionY rotateY").load("views/entradas.html", entradas);
 	});
 	$("#principal").on("click", function(){
-		$("#maincol2").removeClass("transitionY rotateY").addClass("transitionY rotateYY")addClass("transitionY rotateYY").load("views/principal.html", principal);
+		$("#maincol2").removeClass("transitionY rotateY").addClass("transitionY rotateYY").addClass("transitionY rotateYY").load("views/principal.html", principal);
 	});
 	$("#postres").on("click", function(){
 		$("#maincol2").removeClass("transitionY rotateYY").addClass("transitionY rotateY").load("views/postres.html", postres);
