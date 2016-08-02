ef7654a24f228040d4b93bb75c21008bade8bd75
Camilo Andres Martinez <camilo_2510@hotmail.com>
2016-08-02 19:29:45 -0300

    update file

--- javascript/view2.js
+++ javascript/view2.js
@@ -1,6 +1,6 @@
 viewTwo = function(){
 	$("#entradas").on("click", function(){
-		$("#maincol2").removeClass("transitionY rotateY").addClass("transitionY rotateY").load("views/entradas.html", entradas);
+		$("#maincol2").css({"-webkit-transition": "-webkit-transform 2s", "transition": "transform 2s", "-webkit-transform": "rotateY(180deg)", "transform": "rotateY(180deg)"}).load("views/entradas.html", entradas);
 	});
 	$("#principal").on("click", function(){
 		$("#maincol2").removeClass("transitionY rotateY").addClass("transitionY rotateY").load("views/principal.html", principal);
