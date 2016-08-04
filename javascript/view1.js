9130db6aed61617390d610a681827aecf1ff10b4
Camilo Andres Martinez <camilo_2510@hotmail.com>
2016-08-04 19:24:42 -0300

    update

--- javascript/view1.js
+++ javascript/view1.js
@@ -1,7 +1,8 @@
 viewOne = function(){
 	$("#orderHere").on("click", function(){
 		router.evalRoute("view2", viewTwo);
-	})
+	});
 	$("#logIn").on("click", function(){
 		$("#logSpace").load("login.html", login);
+	});
 }
